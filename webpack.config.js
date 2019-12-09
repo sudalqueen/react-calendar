ffc87f02ed824f96aff5ceb5ee60692486d80ec8
sudalqueen <abbey17@cnu.ac.kr>
2019-12-09 16:36:10 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.DEV_PORT || 7777;
+
+module.exports = {
+    entry: [
+      './src/index.js'
+    ],
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.[hash].js'
+    },
+    devServer: {
+        port: port,
+        inline: true,
+        open: true,
+        contentBase: './dist',
+        historyApiFallback: true,
+    },
+    module:{
+        rules:[
+        {
+          test:/\.js$/,
+          exclude:/node_modules/,
+          use:'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  mode: 'local',
+                  localIdentName: '[local]'
+                },
+                import: true,
+                importLoaders: true
+              }
+            }
+          ]
+        },
+        {
+            test: /\.(png|jpg)$/,
+            use: ['file-loader']
+        }
+      ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      // favicon: 'dist/favicon.ico'
+    })
+  ],
+};
