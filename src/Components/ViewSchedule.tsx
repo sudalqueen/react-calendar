6f28144d21cbb47bdefa2f5076757564a434e499
sudalqueen <abbey17@cnu.ac.kr>
2019-12-17 23:06:59 +0900

    달력에 스케쥴을 보여주는 ViewSchedule 객체 작성 중

--- src/Components/ViewSchedule.tsx
+++ src/Components/ViewSchedule.tsx
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
+import {RealSchedule} from "../model/RealSchedule";
+
+import "../style/ViewSchedule.css";
 
 export type ViewScheduleType = {
-
+    schedule: RealSchedule<any>,
+    startY: number,
+    startX: number
 };
 
-function ViewSchedule(){
-
+function ViewSchedule(props: ViewScheduleType){
+    return(
+        <div className="view-schedule">{props.schedule.getTitle()}</div>
+    )
 }
 
 export default ViewSchedule;
