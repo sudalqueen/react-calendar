60ce03960048b851892d9ab6e2a677430991ab1a
sudalqueen <abbey17@cnu.ac.kr>
2019-12-17 14:01:04 +0900

    Calendar.tsx 기본 구현

---
+++ src/Components/Calendar.tsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import DatePicker from "./DatePicker";
+import {RealSchedule} from "../model/RealSchedule";
+
+export type CalendarProps = {
+    schedules: Array<any>
+};
+
+function Calendar({schedules}: CalendarProps) {
+  const [scheduleData, setScheduleData] = useState<Array<RealSchedule<any>>>(schedules);
+
+  useEffect(()=>{
+      scheduleData.map(schedule => {
+          console.log("시작일 : ", schedule.getStartDay());
+      });
+  },[]);
+
+    return (
+        <div>
+            <DatePicker schedules={scheduleData}/>
+        </div>
+    )
+}
+
+export default Calendar;
