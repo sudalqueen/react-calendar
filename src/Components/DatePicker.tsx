8ff46b62cfb9109c605fa964d41dd0232737dedc
sudalqueen <abbey17@cnu.ac.kr>
2019-12-17 13:58:34 +0900

    기본 달력 그려주는 DatePicker

--- src/Components/DatePicker.tsx
+++ src/Components/DatePicker.tsx
@@ -1,46 +1,86 @@
-import React, {useState, useEffect, ElementType} from 'react';
+import React, {useState, useEffect} from 'react';
+
 import DayCell from "./DayCell";
 
 import "../style/DatePicker.css";
 
-const DatePicker = () => {
+type DatePickerProps = {
+  schedules?: any[]
+};
+
+const DatePicker = ({schedules}: DatePickerProps) => {
+    const now = new Date();
+    const currentYear = now.getFullYear();
+    const currentMonth = now.getMonth();
+    const weekNames = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+
     const [year, setYear] = useState(new Date().getFullYear());
     const [month, setMonth] = useState(new Date().getMonth());
     const [day, setDay] = useState(new Date().getDate());
-    const [days, setDays] = useState<React.ReactElement<typeof DayCell>[]>([]);
     const [totalWeeks, setTotalWeeks] = useState<number[]>([]);
-    const weekNames = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+    const [dayMatrix, setDayMatrix] = useState<number[][]>([]);
 
     useEffect(() => {
-        makeCalendar();
-    }, []);
+        makeDayMatrix();
+    }, [year, month]);
 
-    const makeCalendar = () => {
-        let cells:React.ReactElement<typeof DayCell>[] = [];
-        let firstDay = new Date(year, month-2, 1);
-        const lastDate = new Date(year, month -1, 0).getDate();
-        const startWeek = firstDay.getDay() + 1;
-        const totalCells = startWeek + lastDate <= 35 ? 35 : 42;
-        setTotalWeeks(Array.apply(null, new Array(totalCells / 7)).map((item, index) => index + 1));
+    const makeDayMatrix = () => {
+        let matrix: number[][] = [];
 
-        let firstDate = firstDay.getDate();
+        let firstDay = new Date(year, month, 1);
+        const lastDate = new Date(year, month + 1, 0).getDate();
+        const startWeek = firstDay.getDay();
+        const totalCells = startWeek + lastDate + 1 <= 35 ? 35 : 42;
+        const weeks = totalCells / 7;
 
-        for (let i = 0; i < totalCells; i++) {
-            if (i >= startWeek && firstDate < lastDate + 1) {
-                cells.push(<DayCell day={firstDate++}/>)
-            } else {
-                cells.push(<DayCell/>);
+        setTotalWeeks(Array.apply(null, new Array(totalCells / 7)).map((item, index) => index));
+
+        let day = firstDay.getDate();
+
+        for (let i = 0; i < weeks; i++) {
+            matrix[i] = [];
+            for (let j = 0; j < 7; j++) {
+                matrix[i][j] = 0;
             }
         }
-        setDays(cells);
+
+        for (let i = 0; i < weeks; i++) {
+            for (let j = 0; j < 7; j++) {
+                if (i == 0 && j < startWeek || day > lastDate) {
+                    matrix[i][j] = 0;
+                } else {
+                    matrix[i][j] = day++;
+                }
+            }
+        }
+
+        setDayMatrix(matrix);
+    };
+
+    const prevMonth = () => {
+        if (month - 1 === -1) {
+            setYear(year - 1);
+            setMonth(11);
+        } else {
+            setMonth(month - 1);
+        }
+    };
+
+    const nextMonth = () => {
+        if (month + 1 === 12) {
+            setYear(year + 1);
+            setMonth(0);
+        } else {
+            setMonth(month + 1);
+        }
     };
 
     return (
         <div>
-            <header>
-                <span>PREV</span>
-                <span>{year}/{month - 1}</span>
-                <span>NEXT</span>
+            <header className="calendar-header">
+                <span className="calendar-header-prev-btn" onClick={prevMonth}/>
+                <span>{year}/{month + 1}</span>
+                <span className="calendar-header-next-btn" onClick={nextMonth}/>
             </header>
             <div>
                 <div className="calendar-head-week-names">
@@ -49,14 +89,22 @@ const DatePicker = () => {
                     }
                 </div>
                 <div>
-                {
-                    totalWeeks.map(index =>
-                        <div className="calendar-weekend-grid">
-                            {days.map((day, i) => {
-                                if (i <= index * 7 && i > (index - 1) * 7) return day
-                            })}
-                        </div>)
-                }
+                    {
+                        totalWeeks.map(index =>
+                            <div className="calendar-weekend-grid">
+                                {dayMatrix[index].map(dayItem => {
+                                    if (dayItem !== 0) {
+                                        if (year === currentYear && month === currentMonth && dayItem === day) {
+                                            return <DayCell day={dayItem} className="current"/>
+                                        } else {
+                                            return <DayCell day={dayItem}/>
+                                        }
+                                    } else {
+                                        return <DayCell/>
+                                    }
+                                })}
+                            </div>)
+                    }
                 </div>
             </div>
         </div>
