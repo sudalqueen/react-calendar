74624dcf7edd2d2e9ae9f3f326ee1047ff616a06
sudalqueen <abbey17@cnu.ac.kr>
2019-12-17 23:08:06 +0900

    로직 위로 분리하고 프레젠테이셔널 컴포넌트로 수정 - 스케쥴 view 그려줌

--- src/Components/DatePicker.tsx
+++ src/Components/DatePicker.tsx
@@ -5,105 +5,59 @@ import DayCell from "./DayCell";
 import "../style/DatePicker.css";
 
 type DatePickerProps = {
-  schedules?: any[]
+    year: number,
+    month: number,
+    day: number,
+    currentYear: number,
+    currentMonth: number,
+    weekNames: string[],
+    totalWeeks: number[],
+    dayMatrix: number[][],
+    scheduleMatrix: any[][],
+    prevMonth(): void,
+    nextMonth(): void
 };
 
-const DatePicker = ({schedules}: DatePickerProps) => {
-    const now = new Date();
-    const currentYear = now.getFullYear();
-    const currentMonth = now.getMonth();
-    const weekNames = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
-
-    const [year, setYear] = useState(new Date().getFullYear());
-    const [month, setMonth] = useState(new Date().getMonth());
-    const [day, setDay] = useState(new Date().getDate());
-    const [totalWeeks, setTotalWeeks] = useState<number[]>([]);
-    const [dayMatrix, setDayMatrix] = useState<number[][]>([]);
-
-    useEffect(() => {
-        makeDayMatrix();
-    }, [year, month]);
-
-    const makeDayMatrix = () => {
-        let matrix: number[][] = [];
-
-        let firstDay = new Date(year, month, 1);
-        const lastDate = new Date(year, month + 1, 0).getDate();
-        const startWeek = firstDay.getDay();
-        const totalCells = startWeek + lastDate + 1 <= 35 ? 35 : 42;
-        const weeks = totalCells / 7;
-
-        setTotalWeeks(Array.apply(null, new Array(totalCells / 7)).map((item, index) => index));
-
-        let day = firstDay.getDate();
-
-        for (let i = 0; i < weeks; i++) {
-            matrix[i] = [];
-            for (let j = 0; j < 7; j++) {
-                matrix[i][j] = 0;
-            }
-        }
-
-        for (let i = 0; i < weeks; i++) {
-            for (let j = 0; j < 7; j++) {
-                if (i == 0 && j < startWeek || day > lastDate) {
-                    matrix[i][j] = 0;
-                } else {
-                    matrix[i][j] = day++;
-                }
-            }
-        }
-
-        setDayMatrix(matrix);
-    };
-
-    const prevMonth = () => {
-        if (month - 1 === -1) {
-            setYear(year - 1);
-            setMonth(11);
-        } else {
-            setMonth(month - 1);
-        }
-    };
-
-    const nextMonth = () => {
-        if (month + 1 === 12) {
-            setYear(year + 1);
-            setMonth(0);
-        } else {
-            setMonth(month + 1);
-        }
-    };
-
+const DatePicker = (props: DatePickerProps) => {
     return (
         <div>
             <header className="calendar-header">
-                <span className="calendar-header-prev-btn" onClick={prevMonth}/>
-                <span>{year}/{month + 1}</span>
-                <span className="calendar-header-next-btn" onClick={nextMonth}/>
+                <span className="calendar-header-prev-btn" onClick={props.prevMonth}/>
+                <span>{props.year}/{props.month + 1}</span>
+                <span className="calendar-header-next-btn" onClick={props.nextMonth}/>
             </header>
             <div>
                 <div className="calendar-head-week-names">
                     {
-                        weekNames.map(week => <div key={week} className="calendar-head-week-name">{week}</div>)
+                        props.weekNames.map(week => <div key={week} className="calendar-head-week-name">{week}</div>)
                     }
                 </div>
                 <div>
                     {
-                        totalWeeks.map(index =>
-                            <div className="calendar-weekend-grid">
-                                {dayMatrix[index].map(dayItem => {
-                                    if (dayItem !== 0) {
-                                        if (year === currentYear && month === currentMonth && dayItem === day) {
-                                            return <DayCell day={dayItem} className="current"/>
+                        props.totalWeeks.map(index => {
+                            return <div>
+                                <div className="calendar-weekend-grid">
+                                    {props.dayMatrix[index].map(day => {
+                                        if (day !== 0) {
+                                            if (props.year === props.currentYear && props.month === props.currentMonth && day === props.day) {
+                                                return <DayCell day={day} className="current"/>
+                                            } else {
+                                                return <DayCell day={day}/>
+                                            }
                                         } else {
-                                            return <DayCell day={dayItem}/>
+                                            return <DayCell/>
                                         }
-                                    } else {
-                                        return <DayCell/>
-                                    }
-                                })}
-                            </div>)
+                                    })}
+                                    <div className="calendar-schedule-grid">
+                                        {
+                                            props.scheduleMatrix[index].map(viewSchedule =>
+                                                <div className="calendar-schedule-block">{viewSchedule}</div>)
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        })
+
                     }
                 </div>
             </div>
